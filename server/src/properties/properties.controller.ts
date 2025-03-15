2f11246ccaf5849f79f9466df077a88355f28615
bayu <bayuferdiman1@gmail.com>
2025-03-15 14:32:06 +0700

    be: crud properties

--- server/src/properties/properties.controller.ts
+++ server/src/properties/properties.controller.ts
@@ -1,6 +1,17 @@
-import { Controller, Get, NotImplementedException, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotImplementedException,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { PropertiesService } from './properties.service';
 import { CreatePropertiesDto } from './dto/create.properties.dto';
+import { UpdateUserDto } from 'src/users/dto/update.user.dto';
 
 @Controller('properties')
 export class PropertiesController {
@@ -15,7 +26,21 @@ export class PropertiesController {
 
   // CREATE NEW PROP
   @Post()
-  create(@Body() createPropertiesDto: CreatePropertiesDto){
-    return this.propertiesService.create(createPropertiesDto)
+  create(@Body() createPropertiesDto: CreatePropertiesDto) {
+    return this.propertiesService.create(createPropertiesDto);
+  }
+
+  // UPDATE PROP
+  @Put(':id')
+  update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
+    // throw new NotImplementedException();
+    return this.propertiesService.update(+id, updateUserDto);
+  }
+
+  // REMOVE PROP
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    // throw new NotImplementedException();
+    return this.propertiesService.remove(id);
   }
 }
--- server/src/properties/properties.service.ts
+++ server/src/properties/properties.service.ts
@@ -1,5 +1,6 @@
+import { UpdatePropertiesDto } from './dto/update.properties.dto';
 import { CreatePropertiesDto } from './dto/create.properties.dto';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
@@ -18,6 +19,43 @@ export class PropertiesService {
   async create(createPropertiesDto: CreatePropertiesDto) {
     const properties = await prisma.properties.create({
       data: createPropertiesDto,
+    });
+
+    return properties;
+  }
+
+  // update Properties
+  async update(id: number, updatePropertiesDto: UpdatePropertiesDto) {
+    // check id ini ada atau ga
+    const findProp = await prisma.properties.findUnique({
+      where: { id },
+    });
+
+    if (!findProp) {
+      throw new NotFoundException('Properties Not Found');
+    }
+
+    const properties = await prisma.properties.update({
+      where: { id },
+      data: updatePropertiesDto,
+    });
+
+    return properties;
+  }
+
+  // remove Properties
+  async remove(id: number) {
+    // check id ini ada atau ga
+    const findProp = await prisma.properties.findUnique({
+      where: { id },
+    });
+
+    if (!findProp) {
+      throw new NotFoundException('Properties Not Found');
+    }
+
+    const properties = await prisma.properties.delete({
+      where: { id },
     });
 
     return properties;
